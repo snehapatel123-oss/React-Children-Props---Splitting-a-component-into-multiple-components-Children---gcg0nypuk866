969a0355914127eb1de9f7366238ff81c675ea95
snehapatel123-oss <116440431+snehapatel123-oss@users.noreply.github.com>
2023-02-13 21:08:23 +0530

    Update ChildComponent.js

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React , {useState,useEffect} from 'react'
+import '../styles/App.css';
+import ChildComponent from './ChildComponent';
+const App = () => {
+ //code here
+ const [input1, SetInp1] = useState(0)
 
-const ChildComponent = () => {
-//code here
   return (
-    <div id="child">
-      Find The Square <br/>
-     
+    <div id="main">
+      <ChildComponent input={
+       <input id='input' onChange={(e)=>{
+   let ans = e.target.value;
+   ans = Math.pow(ans, 2);
+   SetInp1(ans)
+   
+  }} />}
+ /> 
+    
+      <p id='output'>{input1}</p>
     </div>
   )
 }
 
 
-export default ChildComponent;
+export default App;
